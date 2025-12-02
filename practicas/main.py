a25bb54bcfd0266acd76f18479fee168b4612a8d
AVPabloCSP <a24pabloav@iessanclemente.net>
2025-12-02 20:45:18 +0100

    Create main.py

---
+++ practicas/main.py
@@ -0,0 +1,12 @@
+import math as m
+
+#Comentamos anterior código
+#print('Hola mundo dende Git')
+
+
+#Calcular área dun círculo
+radio = float(input("Introduce o radio: "))
+print("A área do círculo é: ", (radio * radio) * m.pi)
+
+
+print("Versión B-C")
